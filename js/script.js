83e14f87ae3b0fc6cbca345c24b6241d02f951f0
Jason Brown <browniefed@gmail.com>
2015-04-04 14:04:10 -0700

    Add in three line technology.

--- js/script.js
+++ js/script.js
@@ -201,4 +201,23 @@ $(document).ready( function() {
         $replaceLoveQuote.text(bottomQuotes[quoteIndex]);
     }, 2000);
 
+    var navbarmobile = $('.navbar-mobile')[0];
+    var slideout = new Slideout({
+        'panel': document.getElementById('panel'),
+        'menu': document.getElementById('mobilemenu'),
+        'padding': 256,
+        'tolerance': 70,
+        onTranslate: function(param, translate) {
+            navbarmobile.style[param] = navbarmobile.style.transform = translate;
+        },
+        onTransition: function(param, transition) {
+            navbarmobile.style[param] = navbarmobile.style.transition = transition;
+        }
+      });
+
+    $('#hamburger').on('click', function(e) {
+        e.preventDefault();
+        slideout.toggle();
+    });
+
 }())
